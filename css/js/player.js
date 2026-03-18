85aa86281b7819ee3c17e0c21df7b81b8b79ba3b
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 19:05:51 -0300

    Update player.js

--- css/js/player.js
+++ css/js/player.js
@@ -6,29 +6,30 @@ class Player {
         this.speed = 5;
         this.keys = {};
 
-        // Configura os ouvintes de teclado
-        window.addEventListener('keydown', e => this.keys[e.key] = true);
-        window.addEventListener('keyup', e => this.keys[e.key] = false);
+        window.addEventListener('keydown', e => this.keys[e.key.toLowerCase()] = true);
+        window.addEventListener('keyup', e => this.keys[e.key.toLowerCase()] = false);
     }
 
-    update() {
-        // Movimentação básica
-        if (this.keys['ArrowUp'] || this.keys['w']) this.y -= this.speed;
-        if (this.keys['ArrowDown'] || this.keys['s']) this.y += this.speed;
-        if (this.keys['ArrowLeft'] || this.keys['a']) this.x -= this.speed;
-        if (this.keys['ArrowRight'] || this.keys['d']) this.x += this.speed;
+    update(world) {
+        // Movimentação com WASD ou Setas
+        if (this.keys['arrowup'] || this.keys['w']) this.y -= this.speed;
+        if (this.keys['arrowdown'] || this.keys['s']) this.y += this.speed;
+        if (this.keys['arrowleft'] || this.keys['a']) this.x -= this.speed;
+        if (this.keys['arrowright'] || this.keys['d']) this.x += this.speed;
+
+        // Limites do jogador dentro do mundo
+        if (this.x < 0) this.x = 0;
+        if (this.y < 0) this.y = 0;
+        if (this.x > world.width - this.size) this.x = world.width - this.size;
+        if (this.y > world.height - this.size) this.y = world.height - this.size;
     }
 
-    // O método draw agora recebe a 'camera' para saber onde se desenhar na tela
     draw(ctx, camera) {
         ctx.fillStyle = 'red';
+        ctx.fillRect(this.x - camera.x, this.y - camera.y, this.size, this.size);
         
-        // A mágica do mundo aberto: posição real - posição da câmera
-        ctx.fillRect(
-            this.x - camera.x, 
-            this.y - camera.y, 
-            this.size, 
-            this.size
-        );
+        // Pequeno detalhe para saber para onde está olhando (opcional)
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(this.x - camera.x, this.y - camera.y, this.size, this.size);
     }
 }
