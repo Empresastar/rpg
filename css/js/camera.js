cbc65dbb9475527eacdc4cf734f8049cf353b49d
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 18:54:52 -0300

    Update camera.js

--- css/js/camera.js
+++ css/js/camera.js
@@ -1,14 +1,20 @@
 class Camera {
-    constructor(width, height) {
+    constructor(viewWidth, viewHeight) {
         this.x = 0;
         this.y = 0;
-        this.width = width;
-        this.height = height;
+        this.width = viewWidth;
+        this.height = viewHeight;
     }
 
-    follow(target) {
-        // Centraliza o alvo (player) na tela
-        this.x = target.x - this.width / 2;
-        this.y = target.y - this.height / 2;
+    follow(target, world) {
+        // Centraliza a câmera no alvo (player)
+        this.x = target.x + target.size / 2 - this.width / 2;
+        this.y = target.y + target.size / 2 - this.height / 2;
+
+        // Impede a câmera de sair dos limites do mundo (opcional)
+        if (this.x < 0) this.x = 0;
+        if (this.y < 0) this.y = 0;
+        if (this.x > world.width - this.width) this.x = world.width - this.width;
+        if (this.y > world.height - this.height) this.y = world.height - this.height;
     }
 }
