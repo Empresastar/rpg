45d2e942bc3702e26b7cda411e77d2edc21aa480
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 19:23:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+canvas.width = 800;
+canvas.height = 600;
+
+// Instâncias
+const world = new World();
+const camera = new Camera(canvas.width, canvas.height);
+const player = new Player(world.width / 2, world.height / 2);
+
+function update() {
+    player.update(world);
+    camera.follow(player, world);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    world.draw(ctx, camera);
+    player.draw(ctx, camera);
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
